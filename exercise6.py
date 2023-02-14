ba67e6474fa20bea85db785384f38e7ec65d918d
Daria <d-step@mail.ru>
2023-02-14 14:53:36 +0700

    module 6 done

--- exercise6.py
+++ exercise6.py
@@ -1,84 +1,112 @@
-# # EXERCISE 1
-#
-# a = float(input())
-# b = float(input())
-# s = 1/2*a*b
-# print(s)
+# EXERCISE 1
 
-# # EXERCISE 2
-#
-# s = float(input())
-# v1 = float(input())
-# v2 = float(input())
-#
-# v = v1+v2
-# t = s/v
-# print(t)
+a = float(input())
+b = float(input())
+s = 1/2*a*b
+print(s)
 
-# # EXERCISE 3
-#
-#
-# a = float(input())
-#
-# if a == 0:
-#     print('Обратного числа не существует')
-# else:
-#     print(a**(-1))
+# EXERCISE 2
 
-# # EXERCISE 4
-#
-# f = float(input())
-#
-# c = 5/9*(f-32)
-# print(c)
+s = float(input())
+v1 = float(input())
+v2 = float(input())
 
-# # EXERCISE 5
-#
-# dog_age = float(input())
-# human_age = 0
-# if dog_age < 3:
-#     human_age = dog_age * 10.5
-# else:
-#     human_age = dog_age * 4 +13
-# print(human_age)
-#
-# # EXERCISE 6
-#
-# num = float(input())
-# new_num = (num - int(num))*10
-# print(int(new_num))
+v = v1+v2
+t = s/v
+print(t)
 
-#
-# # EXERCISE 7
-#
-# num = float(input())
-# new_num = (num - int(num))
-# print(new_num)
-#
-# # EXERCISE 7
-#
-# a,b,c,d,e = int(input()),int(input()),int(input()),int(input()),int(input())
-# numbers = [a,b,c,d,e]
-# max_num = max(numbers)
-# min_num = min(numbers)
-# print(f'Наименьшее число = {min_num}')
-# print(f'Наибольшее число = {max_num}')
+# EXERCISE 3
+
+
+a = float(input())
+
+if a == 0:
+    print('Обратного числа не существует')
+else:
+    print(a**(-1))
+
+# EXERCISE 4
+
+f = float(input())
+
+c = 5/9*(f-32)
+print(c)
+
+# EXERCISE 5
+
+dog_age = float(input())
+human_age = 0
+if dog_age < 3:
+    human_age = dog_age * 10.5
+else:
+    human_age = dog_age * 4 +13
+print(human_age)
+
+# EXERCISE 6
+
+num = float(input())
+new_num = (num - int(num))*10
+print(int(new_num))
+
 
 # EXERCISE 7
-# сделать условие для строки где вводные данные равны
+
+num = float(input())
+new_num = (num - int(num))
+print(new_num)
+
+# EXERCISE 7
+
+a,b,c,d,e = int(input()),int(input()),int(input()),int(input()),int(input())
+numbers = [a,b,c,d,e]
+max_num = max(numbers)
+min_num = min(numbers)
+print(f'Наименьшее число = {min_num}')
+print(f'Наибольшее число = {max_num}')
+
+# EXERCISE 7
+
 a,b,c = int(input()),int(input()), int(input())
 numbers = [a,b,c]
 max_num = max(numbers)
 min_num = min(numbers)
-if a != max_num and a != min_num:
-    print(max_num)
-    print(a)
-    print(min_num)
-elif b != max_num and b != min_num:
-    print(max_num)
-    print(b)
-    print(min_num)
-elif c != max_num and c != min_num:
-    print(max_num)
-    print(c)
-    print(min_num)
+print(max_num)
+print((a+b+c)-(max_num+min_num))
+print(min_num)
+
+
+# EXERCISE 8
+
+num = int(input())
+hundreds = num // 100
+tens = (num - (hundreds * 100)) // 10
+ones = num % 10
+# print(hundreds, tens, ones)
+
+num_list = [hundreds, tens, ones]
+max_num = max(num_list)
+min_num = min(num_list)
+middle_num = (hundreds + tens + ones)-(max_num+min_num)
+if max_num - min_num == middle_num:
+    print('Число интересное')
+else:
+    print('Число неинтересное')
+
+# EXERCISE 9
+
+a1,a2,a3,a4,a5  = float(input()),float(input()),float(input()),float(input()),float(input())
+a1 = abs(a1)
+a2 = abs(a2)
+a3 = abs(a3)
+a4 = abs(a4)
+a5 = abs(a5)
+sum_num = a1 +a2 +a3 +a4 +a5
+print(sum_num)
+
+# EXERCISE 10
+p1 = int(input())
+p2 = int(input())
+q1 = int(input())
+q2 = int(input())
+distance = abs(p1 -q1) + abs(p2 - q2)
+print(distance)
